60620b46e8ac72d825770503b18e99f96dfd5c1e
Greg Uctum <greg@uctum.net>
2020-04-03 21:32:49 -0400

    created dad joke function

---
+++ Go Projects/jokes/main.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"os"
+)
+
+func joke() {
+	client := &http.Client{}
+	req, _ := http.NewRequest("GET", "https://icanhazdadjoke.com/", nil)
+	req.Header.Add("Accept", "application/json")
+	resp, err := client.Do(req)
+	if err != nil {
+		fmt.Println("No Internet")
+		os.Exit(0)
+	}
+	defer resp.Body.Close()
+	body, _ := ioutil.ReadAll(resp.Body)
+	var data map[string]interface{}
+	json.Unmarshal(body, &data)
+	joke := data["joke"]
+	fmt.Println(joke)
+}
